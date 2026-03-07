788cf423fb7799e4360e15bea9127711cdd76a6d
Ilia Snitko <Snitkoilia@gmail.com>
2026-03-07 18:58:28 +0200

    Added temp controller to query and show sell orders from the SQLite file

---
+++ src/EveOnTrader.Web/Controllers/OrdersController.cs
@@ -0,0 +1,28 @@
+﻿using EveOnTrader.Infra.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EveOnTrader.Web.Controllers;
+
+public class OrdersController : Controller
+{
+    private readonly AppDbContext _db;
+
+    public OrdersController(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    // GET: /Orders
+    public async Task<IActionResult> Index()
+    {
+        // Pull a small, recent slice so we don’t load huge tables into memory.
+        var rows = await _db.MarketOrders
+            .AsNoTracking()  //readonly, faster queries
+            .OrderByDescending(o => o.Issued)
+            .Take(200)
+            .ToListAsync();
+
+        return View(rows);
+    }
+}
